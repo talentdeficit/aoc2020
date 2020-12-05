ff3d818bdff857198e1b737a8a3ae3965d1c7eaf
alisdair sullivan <alisdairsullivan@gmail.com>
2020-12-05 15:55:30 -0800

    aoc2020 :: cleanup day five

--- bin/five/run.jl
+++ bin/five/run.jl
@@ -1,25 +1,33 @@
+using Test
+
 input = joinpath(@__DIR__, "input")
 passes = readlines(input)
 
+# our boarding pass is a thinly disguised 10 bit integer
+const factors = [512, 256, 128, 64, 32, 16, 8, 4, 2, 1]
+const translator = Dict('F' => 0, 'L' => 0, 'B' => 1, 'R' => 1) 
+
 function seat(pass)
-  pass = replace(pass, r"F|L" => "0")
-  pass = replace(pass, r"B|R" => "1")
-  row = parse(Int, pass[1:7], base=2)
-  col = parse(Int, pass[8:end], base=2)
-  return row * 8 + col
+  bin = map(c -> translator[c], collect(pass))
+  return sum(bin .* factors)
 end
 
 function findseat(seatids)
   allpossible = collect(minimum(seatids):maximum(seatids))
-  myseat = first(filter(i -> !(i in seatids), allpossible))
-  return myseat
+  return only(filter(i -> !(i in seatids), allpossible))
 end
 
 seatids = map(p -> seat(p), passes)
-sorted = sort(seatids)
 
 p1 = maximum(seatids)
-p2 = findseat(sorted)
+p2 = findseat(seatids)
+
+@assert(p1 == 994)
+@assert(p2 == 741)
+
+@test seat("BFFFBBFRRR") == 567
+@test seat("FFFBBBFRRR") == 119
+@test seat("BBFFBBFRLL") == 820
 
 println("-----------------------------------------------------------------------")
 println("binary boarding -- part one :: $p1")

b58c8a01f3e7ddec514582c27c3d5a6452de466f
alisdair sullivan <alisdairsullivan@gmail.com>
2020-12-05 17:05:54 -0800

    aoc2020 :: clean up day one

--- bin/one/run.jl
+++ bin/one/run.jl
@@ -3,22 +3,38 @@ using DelimitedFiles
 input = joinpath(@__DIR__, "input")
 entries = readdlm(input, Int)
 
-p1 = 0
-p2 = 0
-
-for i in 1:(length(entries))
+function twosum(entries)
+  for i in 1:(length(entries))
     for j in (i + 1):(length(entries))
-        if entries[i] + entries[j] == 2020
-            global p1 = entries[i] * entries[j]
-        end
-        for k in (j + 1):(length(entries))
-            if entries[i] + entries[j] + entries[k] == 2020
-                global p2 = entries[i] * entries[j] * entries[k]
-                break
-            end
-        end
+      if entries[i] + entries[j] == 2020
+        return entries[i] * entries[j]
+      end
     end
+  end
+
+  return 0
 end
+
+function threesum(entries)
+  for i in 1:(length(entries))
+    for j in (i + 1):(length(entries))
+      for k in (j + 1):(length(entries))
+        if entries[i] + entries[j] + entries[k] == 2020
+          return entries[i] * entries[j] * entries[k]
+        end
+      end
+    end
+  end
+
+  return 0
+end
+
+
+p1 = twosum(entries)
+p2 = threesum(entries)
+
+@assert(p1 == 876459)
+@assert(p2 == 116168640)
 
 println("-----------------------------------------------------------------------")
 println("report repair -- part one :: $p1")
